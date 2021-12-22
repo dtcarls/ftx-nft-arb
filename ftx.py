91eeda082ec82b96f9a60342d93cdecf61d450a7
dtcarls <deantcarlson@gmail.com>
2021-12-22 10:22:04 -0500

    both parallel. new price targets

--- ftx.py
+++ ftx.py
@@ -1,9 +1,10 @@
 import requests
 import json
-import time
 
 def main():
-        collection = "Parallel"
+    collections = ["Parallel","Parallel Alpha"]
+    for collection in collections:
+        #collection = "Parallel Alpha"
         response = requests.get(
             'https://ftx.us/api/nft/nfts_filtered?startInclusive=0&endExclusive=1000000000&nft_filter_string={"collection":"' + 
             collection + '","nftAuctionFilter":"buyNow","minPriceFilter":null,"maxPriceFilter":null,"seriesFilter":[],"traitsFilter":{},"include_not_for_sale":true}&sortFunc=name_asc')
@@ -14,7 +15,7 @@ def main():
         try:
             last_token_id=''
             for NFT in response.json()['result']['nfts']:
-                # print(NFT)
+
                 #print(NFT['ethTokenId']+" - "+NFT['name']+" - "+str(NFT['offerPrice'])+" "+NFT['quoteCurrency'])
                 token_id = NFT['ethTokenId']
                 ftx_price = NFT['offerPrice']
@@ -22,22 +23,39 @@ def main():
                 if last_token_id != token_id:
                     last_token_id = token_id
                     try:
-                        #print("sleeping")
-                        #time.sleep(5)
                         os_url=base_os_url+asset_contract_address+'/'+token_id
                         response = requests.request("GET", os_url, headers={"Accept": "application/json"})
                         # print(response.json()['last_sale']['total_price'])
-                        os_price = int(response.json()['last_sale']['total_price'])/1000000000000000000
+                        rake = int(response.json()['orders'][0]['taker_relayer_fee'])/10000
+                        last_os_price = int(response.json()['last_sale']['total_price'])/1000000000000000000
+
+                        orders = response.json()['orders']
+                        #side 0 = offer
+                        #side 1 = listing
+                        largest_offer = 0
+                        for order in orders:
+                            if order['side'] == 0:
+                                offer = (int(order['current_bounty'])/int(order['quantity']))/10000000000000000
+                                if offer>largest_offer:
+                                    largest_offer=offer
                         
-                        if os_price>ftx_price:
-                            rake = 0.125 #10% take from parallel, 2.5% opensea
-                            profit = round(os_price-ftx_price,2)
-                            true_profit = round(os_price-(os_price*rake)-ftx_price,2)
+                        if last_os_price>ftx_price:
+                            profit = round(last_os_price-ftx_price,2)
+                            true_profit = round(last_os_price-(last_os_price*rake)-ftx_price,2)
                             print("ARB FOUND!")
                             print("FTX: "+NFT['name']+" - "+str(ftx_price)+" "+NFT['quoteCurrency'])
-                            print("OS : "+NFT['name']+" - "+str(os_price)+" ETH")
+                            print("OS : "+NFT['name']+" - "+str(last_os_price)+" ETH")
                             print("Potential Profit:"+str(profit))
-                            print("True Profit:"+str(true_profit))
+                            print("Profit after rake:"+str(true_profit))
+                        
+                        if largest_offer>ftx_price:
+                            profit = round(largest_offer-ftx_price,2)
+                            true_profit = round(largest_offer-(largest_offer*rake)-ftx_price,2)
+                            print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ARB FOUND! XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                            print("FTX: "+NFT['name']+" - "+str(ftx_price)+" "+NFT['quoteCurrency'])
+                            print("OS : "+NFT['name']+" - "+str(largest_offer)+" ETH")
+                            print("Potential Profit:"+str(profit))
+                            print("Profit after rake:"+str(true_profit))
 
                     except json.decoder.JSONDecodeError:
                         print("Can't reach opensea.")
